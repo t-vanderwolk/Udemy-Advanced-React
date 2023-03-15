867d5e3c724461360bc33809a8b0665f13c3e4a6
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-15 10:02:30 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Starter from './tutorial/07-useRef/starter/01-useRef-basics';
+
+function App() {
+  return (
+    <div className='container'>
+     <Starter />
+    </div>
+  );
+}
+
+export default App;
