615cbbd7d7fb024b6be9c62bafed6d1b6c246cd4
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-16 11:59:18 -0700

    useState object example - complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import Starter from './tutorial/01-useState/starter/02-useState-basics';
-import Starter from "./tutorial/01-useState/starter/03-useState-array";
+import Starter from "./tutorial/01-useState/starter/04-useState-object";
 function App() {
   return (
     <div className='container'>
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,36 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+
+
+  const [person, setPerson]= useState({
+    name:'Taylor',
+    age:22,
+    hobby:'full stack developwe'
+  })
+
+
+const displayPerson = () => {
+// setPerson({
+//   name:'Trish',
+//   age:22,
+//   hobby: 'backend developer'
+// });
+// setPerson({name: 'Ava'})
+setPerson({...person, name: "Ava"});
+};
+
+  return <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys : {person.hobby}</h4>
+      <button onClick={(displayPerson)} className='btn'>
+        show person 
+      </button>
+     </>
+  
+  
 };
 
 export default UseStateObject;
