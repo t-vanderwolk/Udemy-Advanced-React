b7e3eac642e6512d4edaef8e5cebeaec4c405702
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-17 10:48:55 -0700

    useState gotcha

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 const UseStateGotcha = () => {
- const [value, setValue] = useState(0)
+ const [value, setValue] = useState(0);
 
  const handleClick = () => {
-  setValue((currentState)=> {
-    const newState = currentState +1;
-    return newState
-;
-  });
-  // console.log(value)
- };
+  setTimeout(() => {
+ console.log('clicked the button')
+    setValue((currentState) => {
+return currentState + 1;
+    });
+  }, 3000);
+};
   return <div>
     <h1>{value}</h1>
     <button type="button" className="btn" onClick={handleClick}>increase</button>
