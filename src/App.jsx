daa751af02b7ccd0fa99deb1758594ccec236dd7
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-19 12:16:24 -0700

    useEffect fetch challenge - complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import Starter from './tutorial/01-useState/starter/02-useState-basics';
-import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects"
+import Starter from "./tutorial/02-useEffect/starter/04-fetch-data"
+import { useState, useEffect } from "react";
+
+
+
+
 function App() {
   return (
     <div className='container'>
      <Starter />
- 
-    </div>
+  </div>
   );
 }
 
