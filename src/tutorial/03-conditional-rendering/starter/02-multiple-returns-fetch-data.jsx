28b7a061f9bbd8c5c838f4e5f038a1bd174c662b
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-25 09:28:53 -0700

    order matters / fetch function location

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -4,30 +4,30 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnsFetchData = () => {
   const [isLoading,setIsLoading] =useState(true);
   const [isError, setIsError] = useState(false);
+
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const resp = await fetch(url);
-        if(!resp.ok){
-          setIsError(true)
-          setIsLoading(false);
-          return
-        }
-        const user = await resp.json();
-        // console.log(resp);
-        // console.log(user);
-        setUser(user)
-      } catch (error) {
-        setIsError(true);
-        // fetch only cares about network errors
-        // will work with axios
-        console.log(error);
-      }
-      setIsLoading(false);
-    };
+  const fetchUser =async () => {
+try{
+  const resp = await fetch(url);
+  if(!resp.ok){
+    setIsError(true);
+    setIsLoading(false);
+    return;
+  }
+  const user = await resp.json();
+
+  setUser(user);
+}catch (error) {
+  setIsError(true);
+  console.log(error);
+
+}
+setIsLoading(false);
+  };
+  useEffect(()=> {
     fetchUser();
+
   }, []);
 
 
@@ -38,13 +38,13 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error</h2>
   }
 
-
+  const {avatar_url, name, bio, company} = user;
 
   return <div>
-    <img style={{width: '150px', borderRadius:'25px'}} src={user.avatar_url} alt={user.name}/>
+    <img style={{width: '150px', borderRadius:'25px'}} src={avatar_url} alt={name}/>
     <h2>{user.name}</h2>
-    <h4>works at {user.company}</h4>
-    <p>{user.bio}</p>
+    <h4>works at {company}</h4>
+    <p>{bio}</p>
   </div>;
 };
 export default MultipleReturnsFetchData;
