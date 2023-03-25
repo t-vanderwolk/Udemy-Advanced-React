764aa920d93d411cee3658d9d0d590d7346a896e
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-25 12:10:13 -0700

    short curcuit - Overview

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+//falsy
+const [text, setText] = useState('')
+//truthy
+const [name, setName] = useState('taylor')
+
+const codeExample = text || 'hello world'
+
+  return <div>
+    <h4>Falsy OR : {text || 'hello world'}</h4>
+    <h4>Falsy AND : {text && 'hello world'}</h4>
+    <h4>Truthy OR : {name || 'hello world'}</h4>
+    <h4>Truthy AND : {name && 'hello world'}</h4>
+{codeExample}
+  </div>;
 };
 export default ShortCircuitOverview;
