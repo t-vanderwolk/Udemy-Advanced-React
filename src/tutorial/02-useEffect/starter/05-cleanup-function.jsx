ab919f14c5aa479f0bf744eec03eac5605a7b996
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-03-31 08:57:38 -0700

    useEffect - Timer Example

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 
 const CleanupFunction = () => {
 
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+  console.log('render');
   return(
      <div>
 <button className="btn" onClick={() => setToggle(!toggle)}>toggle component</button>
@@ -14,9 +15,16 @@ const CleanupFunction = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-console.log('hmmm... this is interesting')
-  }, [])
-  return <h1>hello there!</h1>
+// console.log('hmmm... this is interesting');
+const intId = setInterval(() => {
+// console.log('hello from interval')
+},1000);
+return () => {
+  clearInterval(intId) 
+  console.log('cleanup')
 }
+  }, []);
+  return <h1>hello there!</h1>
+};
 
 export default CleanupFunction;
