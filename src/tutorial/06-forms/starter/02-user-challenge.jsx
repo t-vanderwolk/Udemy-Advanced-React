a24e6361d7d9245d7619bccffe1342a8cea4ef8a
Taylor Vanderwolk <taylorvanderwolk@Taylors-MBP.localdomain>
2023-04-29 15:07:28 -0700

    Remove User

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -15,6 +15,11 @@ const updatedUsers = [...users,newUser]
 setUsers(updatedUsers);
   setName('');
  };
+
+ const removeUser = (id) => {
+  const updatedUsers = users.filter((person) => person.id !== id);
+setUsers(updatedUsers);
+ }
   return (
  <div>
       <form className='form' onSubmit={handleSubmit}>
@@ -37,6 +42,7 @@ setUsers(updatedUsers);
         return(
           <div key={user.id}>
             <h4>{user.name}</h4>
+            <button onClick={()=> removeUser(user.id)} className='btn'>remove</button>
           </div>
         )
       })}
