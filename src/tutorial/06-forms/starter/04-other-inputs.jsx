0b5c58f8d56511f0784c528e4fc4ce0e6dce76f3
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-05-07 16:02:08 -0400

    Checkbox Input

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked)
+  }
   return (
     <div>
       <form className='form'>
@@ -8,6 +13,10 @@ const OtherInputs = () => {
         {/* name */}
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='shipping'> Free Shipping </label>
+          <input type='checkbox'name='shipping' id='shipping' 
+          checked={shipping}
+          onChange={handleShipping}
+          />
         </div>
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='framework' className='form-label'>
